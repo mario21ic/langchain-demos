39a1a6483e901450753681eb76ec30657be09f21
Mario IC <mario21ic@gmail.com>
2023-10-18 22:58:52 -0500

    feat(global) adding agent examples

--- 3-chains/4-mathChain.py
+++ 3-chains/4-mathChain.py
@@ -3,12 +3,10 @@ Genera un code python para luego evaluarlo
 """
 
 import os
-from langchain import LLMChain, OpenAI, PromptTemplate
+from langchain import OpenAI, LLMMathChain
 
 API = os.environ['OPENAI_API_KEY']
 llm = OpenAI(openai_api_key=API)
 
-# MathChain
-from langchain import LLMMathChain
 cadena_mate = LLMMathChain(llm=llm, verbose=True)
-cadena_mate.run("Cuanto es 432*12-32+32?")
+print(cadena_mate.run("Cuanto es 432*12-32+32?"))
